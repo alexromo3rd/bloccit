e4d43504df11a0f6b999e60deb7908c1a57cf92d
Alex Romo III <alexromo3rd@gmail.com>
2017-03-23 17:10:08 +0000

    Assignment 18 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,14 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
+  	
+  	@posts.each do |post|
+      if @posts.index(post) == 0
+        post.title = "SPAM"
+      elsif (@posts.index(post) % 5) == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
