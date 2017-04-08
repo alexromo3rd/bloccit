9db4b9c21d9646553b52b13b9a8a26f3b235d793
Alex Romo III <alexromo3rd@gmail.com>
2017-04-08 07:29:18 -0700

    working on sponsored posts controller spec

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe SponsoredPostsController, type: :controller do
 
+  let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:my_sponsored_post) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 99) }
+  
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id
       expect(response).to have_http_status(:success)
     end
   end
