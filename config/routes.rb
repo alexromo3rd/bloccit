b5087c931f402fa4b6ea89aa411c55dea2d7c384
Alex Romo III <ayromo08@gmail.com>
2017-04-09 21:42:48 -0700

    Checkpoint 24 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :topics do
-  	resources :posts, except: [:index]
+    resources :posts, except: [:index]
   end
+
+  # #8
+  resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
 
