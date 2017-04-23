429e89c9b48c6da628b0ff7b169d2221d3b2bcf4
Alex Romo III <alexromo3rd@gmail.com>
2017-04-23 08:41:35 -0700

    Checkpoint 27 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,28 @@
 class PostsController < ApplicationController
+  # #12
+  before_action :require_sign_in, except: :show
+
   def show
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
     @topic = Topic.find(params[:topic_id])
-  	@post = Post.new
+    @post = Post.new
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
+    # #14
+    @post.user = current_user
 
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
-    	flash.now[:alert] = "There was an error saving the post. Please try again."
-    	render :new
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
     end
   end
 
@@ -30,8 +32,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -41,10 +42,10 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
-    
+
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
       redirect_to @post.topic
@@ -52,5 +53,11 @@ class PostsController < ApplicationController
       flash.now[:alert] = "There was an error deleting the post."
       render :show
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
