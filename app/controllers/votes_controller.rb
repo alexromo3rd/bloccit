b5fe36c038e2d587aa0048806b6ccbfa8926e25c
Alex Romo III <alexromo3rd@gmail.com>
2017-04-26 21:57:29 -0700

    Updated votes controller and votes controller spec for up_vote and down_vote methods

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,31 @@
 class VotesController < ApplicationController
+  # #24
+  before_action :require_sign_in
+
+  def up_vote
+    @post = Post.find(params[:post_id])
+    @vote = @post.votes.where(user_id: current_user.id).first
+
+    if @vote
+      @vote.update_attribute(:value, 1)
+    else
+      @vote = current_user.votes.create(value: 1, post: @post)
+    end
+
+    # #25
+    redirect_to :back
+  end
+
+  def down_vote
+    @post = Post.find(params[:post_id])
+    @vote = @post.votes.where(user_id: current_user.id).first
+
+    if @vote
+      @vote.update_attribute(:value, -1)
+    else
+      @vote = current_user.votes.create(value: -1, post: @post)
+    end
+
+    redirect_to :back
+  end
 end
