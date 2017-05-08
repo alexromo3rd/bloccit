265c9b1006a6c361b58aa7549fd0ca3c8fe54546
Alex Romo III <alexromo3rd@gmail.com>
2017-05-08 06:28:35 -0700

    Scoping posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require 'rails_helper' 
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { create(:user) }
   
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
@@ -25,28 +25,28 @@ RSpec.describe User, type: :model do
 
   describe "attributes" do
     it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+      expect(user).to have_attributes(name: user.name, email: user.email)
     end
-    # #1
+    
     it "responds to role" do
       expect(user).to respond_to(:role)
     end
-    # #2
+   
     it "responds to admin?" do
       expect(user).to respond_to(:admin?)
     end
-    # #3
+  
     it "responds to member?" do
       expect(user).to respond_to(:member?)
     end
   end
   
   describe "roles" do
-    # #4
+ 
     it "is member by default" do
       expect(user.role).to eql("member")
     end
-    # #5
+
     context "member user" do
       it "returns true for #member?" do
         expect(user.member?).to be_truthy
@@ -56,7 +56,7 @@ RSpec.describe User, type: :model do
         expect(user.admin?).to be_falsey
       end
     end
-    # #6
+
     context "admin user" do
       before do
         user.admin!
@@ -92,15 +92,27 @@ RSpec.describe User, type: :model do
     end
 
     it "returns 'nil' if the user has not favorited the post" do
-      # #1
+
       expect(user.favorite_for(@post)).to be_nil
     end
 
     it "returns the appropriate favorite if it exists" do
-      # #2
+
       favorite = user.favorites.where(post: @post).create
-      # #3
+
       expect(user.favorite_for(@post)).to eq(favorite)
+    end
+  end
+
+  describe ".avatar_url" do
+
+    let(:known_user) { create(:user, email: "blochead@bloc.io") }
+
+    it "returns the proper Gravatar url for a known email entity" do
+
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)
     end
   end
 end
